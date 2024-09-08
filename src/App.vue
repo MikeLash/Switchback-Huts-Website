62e453fc4ce045ddb42408c7c2db65d094bf1778
Mike <mike.jlash5@gmail.com>
2024-09-08 10:43:03 -0700

    updated components and added image asset

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <img src="./assets/hut.png">
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <div id="app">
     <home-Page-View />
     <router-view />
--- src/components/headerBar.vue
+++ src/components/headerBar.vue
@@ -1,16 +1,36 @@
 <template>
-    <header>
-      <div class="logo">
-        <img src="logo.png" alt="Logo">
-      </div>
-      <nav>
-        <ul>
-          <li><router-link to="/huts">Huts</router-link></li>
-          <li><router-link to="/winter-fun">Winter Fun</router-link></li>
-          <li><router-link to="/services">Services</router-link></li>
-          <li><router-link to="/logistics">Logistics</router-link></li>
-          <li><router-link to="/contact">Contact</router-link></li>
-        </ul>
-      </nav>
-    </header>
-  </template>
+  <header>
+    <div class="hut">
+      <img src="hut.png" alt="">
+    </div>
+    <nav>
+      <ul>
+        <li><router-link to="/huts">Huts</router-link></li>
+        <li><router-link to="/winter-fun">Winter Fun</router-link></li>
+        <li><router-link to="/services">Services</router-link></li>
+        <li><router-link to="/logistics">Logistics</router-link></li>
+        <li><router-link to="/contact">Contact</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script></script>
+
+<style scoped>
+.hut image {
+  width: 10;
+  height: 10;
+}
+ul {
+  display: flex;
+  list-style: none;
+}
+nav {
+  display: flex;
+  justify-content: center; /* Center the navigation menu */
+}
+li {
+  margin: 0 10px;
+}
+</style>
