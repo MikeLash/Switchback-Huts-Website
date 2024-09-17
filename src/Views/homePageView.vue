f58517791604b6d274a83779fa551fea0f253b1c
Mike <mike.jlash5@gmail.com>
2024-09-17 10:02:52 -0700

    added 3d mapbox map to homePageView.vue

--- src/Views/homePageView.vue
+++ src/Views/homePageView.vue
@@ -6,6 +6,9 @@
         <p>Discover the beauty of [Your Location] and plan your winter adventure.</p>
         <button>Explore Now</button>
       </div>
+      <div>
+        <iframe width='100%' height='900px' src="https://api.mapbox.com/styles/v1/mikelash/ckrz32gte0xeg17midy435ihv.html?title=false&access_token=pk.eyJ1IjoibWlrZWxhc2giLCJhIjoiY2xwaWg4YXY3MGM1eTJrcXM4c2Q0MGZxYyJ9.JYGddBoNM7uBA5BOzS3Gfw&zoomwheel=false#11.18/43.5715/-114.7151/0/45" title="Swiss ski" style="border:none;"></iframe> 
+      </div>
       <section class="featured-huts">
         <h2>Featured Huts</h2>
         <div class="hut-cards">
